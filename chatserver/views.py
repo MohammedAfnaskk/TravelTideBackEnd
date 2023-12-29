ab742c440bfe8f9a6e9117ab143ab1c5b3d72c85
Mohammed Afnas <afnaskkvlr@gmail.com>
2023-12-29 21:37:04 +0530

    syntex error solved

--- chatserver/views.py
+++ chatserver/views.py
@@ -21,9 +21,7 @@ class PreviousMessagesView(ListAPIView):
     def get_queryset(self):
         user1 = int(self.kwargs['user1'])
         user2 = int(self.kwargs['user2'])
-
-        thread_suffix = f"{user1}_{
-            user2}" if user1 > user2 else f"{user2}_{user1}"
+        thread_suffix = f"{user1}_{user2}" if user1 > user2 else f"{user2}_{user1}"
         thread_name = 'chat_'+thread_suffix
         queryset = Message.objects.filter(
             thread_name=thread_name
