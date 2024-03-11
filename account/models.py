f4349178906a2652ec184b7b5a2d80166cbf70fb
Mohammed Afnas <afnaskkvlr@gmail.com>
2024-03-11 21:50:50 +0530

    superuser database manage

--- account/models.py
+++ account/models.py
@@ -2,7 +2,32 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
 from django.core.validators import MaxValueValidator
 
- 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("role", 'admin')
+
+        if extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True.")
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+
+
 class CustomUser(AbstractUser):
     USER_ROLES= (
         ('user','User'),
